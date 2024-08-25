3a5b750a9f23d40928ea64d58633a33bc577d600
Asteral <134489068+Asteral1@users.noreply.github.com>
2024-08-25 10:48:32 +0000

    modified:   instructions/activity.js

---
+++ instructions/activity.js
@@ -0,0 +1,44 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+    name: 'activity',
+    description: 'Sets the bot\'s activity.',
+    execute(message, args, config) {
+        if (args.length < 2) {
+            return message.reply(`❕ Usage: [prefix]activity <type> <activity>`);
+        }
+
+        const [typeArg, ...activityArgs] = args;
+        const activityName = activityArgs.join(' ');
+
+        let activityType;
+
+        switch (typeArg.toLowerCase()) {
+            case 'playing':
+                activityType = ActivityType.Playing;
+                break;
+            case 'streaming':
+                activityType = ActivityType.Streaming;
+                break;
+            case 'listening':
+                activityType = ActivityType.Listening;
+                break;
+            case 'watching':
+                activityType = ActivityType.Watching;
+                break;
+            case 'competing':
+                activityType = ActivityType.Competing;
+                break;
+            case 'custom':
+                activityType = ActivityType.Custom
+            default:
+                return message.reply('❗ Invalid activity type. Valid types are: playing, streaming, listening, watching, competing, custom.');
+        }
+
+        message.client.user.setPresence({
+            activities: [{ name: activityName, type: activityType }],
+        });
+
+        message.reply(`✔ Activity set to: **${typeArg} ${activityName}**`);
+    },
+};
